809c76e785b34053f8b066e76f6ce2dfc960aa0d
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-25 00:22:38 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,12 +10,12 @@ class Author
   end
 
   def add_post(post)
-    post.author == self
+    post.author = self
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
-    post.author == self
+    post.author = self
   end
 
   def posts
