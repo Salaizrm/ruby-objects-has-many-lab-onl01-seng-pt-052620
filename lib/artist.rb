00f196a0008db324b84fc3a83beaed1d4dbba534
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-25 00:12:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,11 +23,11 @@ class Artist
   end
 
   def songs
-  Song.all.select {|song| song.artist == self}
-end
+    Song.all.select {|song| song.artist == self}
+  end
 
-def self.song_count
-  Song.all.count
-end
+  def self.song_count
+    Song.all.count
+  end
 
 end
