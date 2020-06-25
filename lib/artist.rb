293ba6e89630321e0ad4d8bf2a3bb34abe2585d5
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-25 00:10:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,16 @@
 class Artist
 
-  attr_accessor :name, :songs
+  attr_accessor :name
 
+  @@all = []
 
   def initialize(name)
     @name = name
-    @songs = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def add_song(song)
